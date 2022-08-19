0413873a3498ca0f410cefb808d023c01f7c37df
Janice-M <janiceink001@gmail.com>
2022-08-19 16:29:34 +0300

    user

--- models/user.js
+++ models/user.js
@@ -1,3 +1,20 @@
 const mongoose = require ('mongoose');
 const Schema = mongoose.Schema;
 const { isEmail } = require ('validator');
+
+const UserSchema = new Schema ({
+    name: {
+        type:String, 
+        required: true
+
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter an email'],
+        unique: true,
+        lowercase: true,
+        validate: [isEmail, 'Please enter valid email']
+
+    },
+    
+})
