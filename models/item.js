8f0df9fc5ea7432869582134303abfec976a406c
Janice-M <janiceink001@gmail.com>
2022-08-22 17:48:39 +0300

    items

--- models/item.js
+++ models/item.js
@@ -2,6 +2,20 @@ const mongoose = require ('mongoose');
 const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema ({
+    title: {
+        type: String,
+        required: true
+    },
+    description{
+        type: String,
+        required: true,
 
+    }, 
     
+    price: {
+        type: Number,
+        required: true
+    },
+    
+
 })
