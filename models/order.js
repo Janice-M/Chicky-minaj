0f272e0f676790dafb3e029061e5556ced92f717
Janice-M <janiceink001@gmail.com>
2022-08-22 18:06:46 +0300

    order

--- models/order.js
+++ models/order.js
@@ -2,5 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
  
 const OrderSchema = new Schema ({
-    
-})
+
+    userId: {
+        type: String,
+    },
+    items: [
+        {
+            productId: {
+                type: String,
+            },
+            name: {
+                type: String,
+                quantity
+            }
+        }
+    ]
+
+});
